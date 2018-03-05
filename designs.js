83502efdcccc56803de1d906d110685ed09cd677
William George Cook <wgeorgecook@gmail.com>
2018-03-04 19:35:04 -0800

    Full functionality, I'm happy with the design

--- designs.js
+++ designs.js
@@ -12,20 +12,37 @@
 $(function () {
     // sets the grid
     $("#sizePicker").on("submit", function (event) {
-        removeGrid(); // start fresh
+        // removeGrid(); // start fresh
+        whileRemoveGrid(); // start fresh with a while grid
         makeGrid(); // draw new grid based on user input
         event.preventDefault();
-            // picks the highlighted cell and sets the color
+
+        // picks the highlighted cell and sets the color
         $("td").on("click", function() {
             console.log("I've been clicked!");
             $(this).css("background-color", getColor());
+
+            /*
+
+            // can remove an instruction by comparing values. Would require a single click each time.
+            if ( getBgColorHex($this) === $("#colorPicker").val() ) {
+                console.log("Same color. Reset to default.");
+                $(this).css("background-color", "");
+            }
+            } else {
+                console.log("New color. Set new value.");
+                $(this).css("background-color", getColor());
+            };
+
+            */
         });
+            
 
         // removes on double click
         $("td").on("dblclick", function() {
             console.log("I've been double-clicked!");
             $(this).css("background-color", "");
-        });
+       });
     }); 
 
 });
@@ -62,6 +79,17 @@ function removeGrid() {
     
 }
 
+function whileRemoveGrid() {
+    // Critera asks for a while loop
+    // so here it is
+    var grid, t;
+    grid = $("table");
+    t = grid.children().length;
+    while (t > 0) {
+        grid.children().first().remove();
+        t --;
+    };
+};
 function getColor() {
     //variables
     var selectColor, currentColor, gridCell;
@@ -69,3 +97,22 @@ function getColor() {
     currentColor = selectColor.val();
     return currentColor;
 };
+
+function getBgColorHex(elem){
+    /*
+    Will convert the rgba value from .css('background-color) into hex to 
+    compare against $("#colorPicker").val()
+    Courtesey shaik from 
+    https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+    */
+    var color = elem.css('background-color')
+    var hex;
+    if(color.indexOf('#')>-1){
+        //for IE
+        hex = color;
+    } else {
+        var rgb = color.match(/\d+/g);
+        hex = '#'+ ('0' + parseInt(rgb[0], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) + ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2);
+    }
+    return hex;
+}
